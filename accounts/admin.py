b8396a9c9b69fe9d87de9d21887f8fd78e0c1219
dmitryhusev <dimtri.gusev@gmail.com>
2023-10-26 22:34:00 +0200

    init

---
+++ accounts/admin.py
@@ -0,0 +1,23 @@
+from .models import Profiles
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+class UserAdmin(UserAdmin):
+    model = Profiles
+    list_display = ("email", "id", "first_name", "last_name", "is_superuser", "is_active")
+    list_filter = ()
+    ordering = ("-id",)
+    fieldsets = ()
+    add_fieldsets = (
+            (
+                None,
+                {
+                    'classes': ('wide',),
+                    'fields': ('email', 'password1', 'password2'),
+                },
+            ),
+        )
+
+admin.site.register(Profiles, UserAdmin)
